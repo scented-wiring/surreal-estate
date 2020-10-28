c6b82a939cfa6e98aab4aeac0c83b0176f4ccbf2
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-10-28 20:43:01 +0000

    add mock data for PropertyCard

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "../styles/Properties.css";
+import PropertyCard from "../components/PropertyCard";
 
-const Properties = () => <div className="properties">Properties Page</div>;
+const Properties = () => (
+  <div className="properties">
+    <PropertyCard
+      title="2 Bed Flat"
+      type="Flat"
+      bathrooms={2}
+      bedrooms={2}
+      price={10000}
+      city="Manchester"
+      email="hello@test.com"
+    />
+  </div>
+);
 
 export default Properties;
