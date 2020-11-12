e108942cc11169bedb7522f8e4cf191e0b42164d
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-11-12 13:48:59 +0000

    Property component displays data added from AddProperty form

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Alert from "../components/Alert";
+import axios from "axios";
 import "../styles/Properties.css";
 import PropertyCard from "../components/PropertyCard";
 
-const Properties = () => (
-  <div className="properties">
-    <PropertyCard
-      title="2 Bed Flat"
-      type="Flat"
-      bathrooms={2}
-      bedrooms={2}
-      price={10000}
-      city="Manchester"
-      email="hello@test.com"
-    />
-  </div>
-);
+const Properties = () => {
+  const [properties, setProperties] = useState([]);
+
+  const [alert, setAlert] = useState({ message: "", isSuccess: false });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/PropertyListing")
+      .then((response) => setProperties(response.data))
+      .catch(() => {
+        setAlert({
+          message: "Could not retrieve properties",
+          isSuccess: false,
+        });
+      });
+  }, []);
+
+  return (
+    <div className="properties">
+      <Alert message={alert.message} success={alert.isSuccess} />
+      {properties.map((property) => (
+        <div key={property.id} className="col">
+          <PropertyCard key={property._id} {...property} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Properties;
