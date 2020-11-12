87a1a8ebcb27fcafdbb2e3b4c34061187209cb56
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-11-12 16:33:40 +0000

    clean up

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Alert from "../components/Alert";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import "../styles/Properties.css";
+import Alert from "../components/Alert";
 import PropertyCard from "../components/PropertyCard";
+import Sidebar from "../components/Sidebar";
 
 const Properties = () => {
   const [properties, setProperties] = useState([]);
@@ -21,11 +23,21 @@ const Properties = () => {
       });
   }, []);
 
+  const { search } = useLocation();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/PropertyListing${search}`)
+      .then(({ data }) => setProperties(data))
+      .catch((err) => console.error(err));
+  }, [search]);
+
   return (
     <div className="properties">
+      <Sidebar />
       <Alert message={alert.message} success={alert.isSuccess} />
       {properties.map((property) => (
-        <div key={property.id} className="col">
+        <div key={property.id} className="card">
           <PropertyCard key={property._id} {...property} />
         </div>
       ))}
