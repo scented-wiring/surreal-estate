3a475c64947151e859ab3fac99ce6f077eb3095b
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-11-13 15:03:28 +0000

    add sort by price

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,55 @@
 import React from "react";
 import "../styles/Sidebar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 
 const Sidebar = () => {
+  const { search } = useLocation();
+
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <div className="sidebar">
       <div className="item">Filter by city</div>
       <div className="item">
-        <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
+        <Link to={buildQueryString("query", { city: "Manchester" })}>
+          Manchester
+        </Link>
       </div>
       <div className="item">
-        <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
+        <Link to={buildQueryString("query", { city: "Leeds" })}>Leeds</Link>
       </div>
       <div className="item">
-        <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
+        <Link to={buildQueryString("query", { city: "Sheffield" })}>
+          Sheffield
+        </Link>
       </div>
       <div className="item">
-        <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
+        <Link to={buildQueryString("query", { city: "Liverpool" })}>
+          Liverpool
+        </Link>
+      </div>
+      <div className="item">Sort by</div>
+      <div className="item">
+        <Link to={buildQueryString("sort", { price: -1 })}>
+          Price - Descending
+        </Link>
+      </div>
+      <div className="item">
+        <Link to={buildQueryString("sort", { price: 1 })}>
+          Price - Ascending
+        </Link>
       </div>
     </div>
   );
