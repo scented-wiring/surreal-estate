5524446431475cab01a0bcd1a7de121cee12c75f
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-11-21 17:33:40 +0000

    add App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render, wait } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "../components/App";
 
-test("renders h2 element", () => {
-  const { getByText } = render(<App />);
-  const h2 = getByText("Surreal Estate");
-  expect(h2).toBeInTheDocument();
+beforeAll(() => {
+  const script = document.createElement("script");
+  document.body.appendChild(script);
+});
+
+describe("App", () => {
+  it("renders a component", () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    expect(asFragment).toMatchSnapshot();
+  });
+
+  it("renders a Navbar", () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const title = getByText("Surreal Estate");
+    expect(title).toBeInTheDocument();
+  });
 });
